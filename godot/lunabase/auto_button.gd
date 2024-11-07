70f4958bdfe8e0cce3270d7a808e228de1a19490
Tysty <tylerstyle05@gmail.com>
2024-11-06 18:23:38 -0700

    State text color indicator
    state text turns red if robot is stopped or is in auto when joystick is moved

--- godot/lunabase/auto_button.gd
+++ godot/lunabase/auto_button.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	grab_focus()
 
 func _on_pressed() -> void:
+	Lunabot.current_state=Lunabot.State.AUTO
 	$"../Buttons Label".text = "Current Stage: Auto"
 	$"../../StateInfoTabContainer".current_tab=0
 	#Lunabot.runautonomous()
--- godot/lunabase/lunabot.gd
+++ godot/lunabase/lunabot.gd
@@ -1,5 +1,12 @@
 extends LunabotConn
 
+enum State{
+	AUTO,
+	TELEOP,
+	STOPPED
+}
+
+var current_state:State = State.STOPPED
 
 func _ready() -> void:
 	await get_tree().create_timer(4.0).timeout
