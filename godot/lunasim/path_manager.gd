e6a7106504363e9582af7063fa51de6af3dcd565
Tyler Chin <tylerstyle05@gmail.com>
2024-11-08 13:39:18 -0700

    Changed from vector2 to vector3
    create_path function now takes in a Vector3 array instead of Vector2

--- godot/lunasim/path_manager.gd
+++ godot/lunasim/path_manager.gd
@@ -1,7 +1,6 @@
 extends Path3D
 
-@export var test_path:Array[Vector2]
-@export var height = 0.25
+@export var test_path:Array[Vector3]
 var path_mat : Material=preload("res://path_material.tres")
 @onready var robot = $"../Robot"
 @onready var line_mesh = $CSGPolygon3D
@@ -12,19 +11,20 @@ func _ready() -> void:
 	line_mesh.material=path_mat
 	create_path(test_path)
 
-func create_path(path:Array[Vector2]):
-	self.curve.add_point(Vector3(robot.position.x, height, robot.position.z))
+func create_path(path:Array[Vector3]):
+	self.curve.add_point(Vector3(robot.position.x, robot.position.y+0.5, robot.position.z))
 	for i in path.size():
-		self.curve.add_point(Vector3(path[i].x, height, path[i].y))
-		place_marker(path[i].x,path[i].y,str(i))
+		var point_pos=Vector3(path[i].x, path[i].y, path[i].z)
+		self.curve.add_point(point_pos)
+		place_marker(point_pos,str(i))
 
 #places cylinder maker for every point in path
 # x and z parameters are for marker position
 #num is for identifying which marker is being placed
-func place_marker(x:float,z:float,num:String):
+func place_marker(pos:Vector3,num:String):
 		var marker = CSGCylinder3D.new()
-		marker.position=Vector3(x, height/2, z)
-		marker.height=height
+		marker.position=Vector3(pos.x,(pos.y/2)+0.05,pos.z)
+		marker.height=pos.y
 		marker.radius=0.05
 		marker.name = str("marker",num)
 		marker.material=path_mat
