6a42dac9ae9b5a03ceb97e24b1f754582b1cf681
Najman Husaini <najmanhusaini20@gmail.com>
2024-11-14 22:19:20 -0700

    clustering test

--- misc/thalassic/src/main.rs
+++ misc/thalassic/src/main.rs
@@ -1,11 +1,14 @@
 use thalassic::Clusterer;
 
 fn main() {
-    let clusters: Vec<_> = Clusterer::new(0.4, 0.25, 4, 6, |d| (d * 3.0).round() as usize).cluster(&[
-        0.0, 0.1, 0.2, 0.3,
-        0.4, 0.5, 0.6, 0.7,
-        0.8, 0.9, 1.0, 1.1,
-        1.2, 1.3, 1.4, 1.5,
-    ]).collect();
+    let img = image::load_from_memory(include_bytes!("heatmap.png")).unwrap().to_luma32f();
+    let heatmap: Vec<_> = img.into_vec().into_iter().map(|n| n as f64).collect();
+    let clusters: Vec<_> = Clusterer::new(0.1, 0.05, 64, 2, |d| (d * 3.0).round() as usize).cluster(&heatmap).collect();
+    
+    // (0.6, 0.5)
+    // (1.5, 1.15)
+    // (1.85, 0.25)
+    // (2.6, 0.85)
+
     println!("{:#?}", clusters);
 }
