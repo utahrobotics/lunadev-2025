7ff30ece07e6031940b64cf1f58bbef28268835c
Tyler Chin <tylerstyle05@gmail.com>
2024-11-14 18:18:17 -0700

    reset path
    path doesn't duplicate when create_path function is called

--- godot/lunasim/path_manager.gd
+++ godot/lunasim/path_manager.gd
@@ -4,14 +4,14 @@ extends Path3D
 var path_mat : Material=preload("res://path_material.tres")
 @onready var robot = $"../Robot"
 @onready var line_mesh = $CSGPolygon3D
+var markers :=[]
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	line_mesh.material=path_mat
-	create_path(test_path)
+	#create_path(test_path) # Comment this when implementing with rust
 
 func create_path(path:Array[Vector3]):
+	self.curve.clear_points()
 	self.curve.add_point(Vector3(robot.position.x, robot.position.y+0.5, robot.position.z))
 	for i in path.size():
 		var point_pos=Vector3(path[i].x, path[i].y, path[i].z)
@@ -22,10 +22,12 @@ func create_path(path:Array[Vector3]):
 # x and z parameters are for marker position
 #num is for identifying which marker is being placed
 func place_marker(pos:Vector3,num:String):
-		var marker = CSGCylinder3D.new()
-		marker.position=Vector3(pos.x,(pos.y/2)+0.05,pos.z)
-		marker.height=pos.y
-		marker.radius=0.05
-		marker.name = str("marker",num)
-		marker.material=path_mat
-		add_child(marker)
+	markers.clear()
+	var marker = CSGCylinder3D.new()
+	marker.position=Vector3(pos.x,(pos.y/2)+0.05,pos.z)
+	marker.height=pos.y
+	marker.radius=0.05
+	marker.name = str("marker",num)
+	marker.material=path_mat
+	add_child(marker)
+	markers.append(marker)
