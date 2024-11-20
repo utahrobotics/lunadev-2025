6e70cc525e99e1eb1bf90bee9981eea533cf79c6
Tyler Chin <tylerstyle05@gmail.com>
2024-11-20 12:41:56 -0700

    image created and edited

--- godot/lunaviz/MapManager.gd
+++ godot/lunaviz/MapManager.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var textbox=$CanvasLayer/VizHBox/DataVBox/Texbox/ScrollContainer/textVBox
-
+@onready var map_texture=$"CanvasLayer/VizHBox/ImagePanel/VBoxContainer/MapTexture"
+@onready var map_title= $CanvasLayer/VizHBox/ImagePanel/VBoxContainer/Title
+var current_map = "Depth"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_message("Hello",Color.RED)
+	map_title.text=current_map+" Map"
+	generate_image(64,64)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +23,49 @@ func set_message(msg: String, color: Color):
 	message.add_theme_color_override("font_color",color)
 	message.size_flags_vertical=Control.SIZE_SHRINK_BEGIN
 	textbox.add_child(message)
+
+func generate_image(width:int, height:int):
+	var image = Image.create(width, height, false, Image.FORMAT_RGBA8)
+	for y in range(height):
+		for x in range(width):
+			var color = Color(x / float(width), y / float(height), 0.5, 1.0)
+			image.set_pixel(x,y,color)
+	var texture= ImageTexture.create_from_image(image)
+	map_texture.texture=texture
+	map_texture.set_size(Vector2(64, 64)) 
+	print(map_texture.texture)
+
+
+
+func _on_depth_map_button_down() -> void:
+	current_map="Depth"
+	map_title.text=current_map+" Map"
+	set_message("Depth Map Selected",Color.WHITE)
+
+
+func _on_point_map_button_down() -> void:
+	current_map="Point"
+	map_title.text=current_map+" Map"
+	set_message("Point Map Selected",Color.WHITE)
+
+
+func _on_height_map_button_down() -> void:
+	current_map="Height"
+	map_title.text=current_map+" Map"
+	set_message("Height Map Selected",Color.WHITE)
+
+
+func _on_gradient_map_button_down() -> void:
+	current_map="Gradient"
+	map_title.text=current_map+" Map"
+	set_message("Gradient Map Selected",Color.WHITE)
+
+func _on_obstacle_map_button_down() -> void:
+	current_map="Obstacle"
+	map_title.text=current_map+" Map"
+	set_message("Obstacle Map Selected",Color.WHITE)
+
+
+func _on_send_button_button_up() -> void:
+	print(current_map)
+	set_message(current_map+" Map Sent",Color.WHITE)
