064e617edbdc94c2cdfb866741ea841e2be2ae3d
Najman (Lunaserver) <najmanhusaini20@gmail.com>
2024-12-20 20:38:25 -0700

    remove unused

--- lunaserver-web/lunaserver-web-host/src/main.rs
+++ lunaserver-web/lunaserver-web-host/src/main.rs
@@ -1,58 +1,12 @@
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
 use axum::{
-    extract::{ws::Message, ConnectInfo, Request, State, WebSocketUpgrade},
-    http::{
-        uri::{Authority, Scheme},
-        Uri,
-    },
-    routing::{any, get},
+    extract::{ws::Message, ConnectInfo, Path, WebSocketUpgrade},
+    routing::get,
     Router,
 };
-use reqwest::{Client, Url};
-use tokio::net::UdpSocket;
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpStream, UdpSocket}};
 
-#[axum::debug_handler]
-async fn lunabot_redirect(
-    client: State<Client>,
-    mut request: Request,
-) -> Result<axum::response::Response, String> {
-    let mut uri_parts = axum::http::uri::Parts::default();
-    uri_parts.scheme = Some(Scheme::HTTP);
-    uri_parts.authority = Some(Authority::from_static("127.0.0.1:21141"));
-    uri_parts.path_and_query = request.uri().path_and_query().cloned();
-
-    *request.uri_mut() = Uri::from_parts(uri_parts).unwrap();
-    let mut new_request = reqwest::Request::new(
-        request.method().clone(),
-        Url::parse(&request.uri().to_string()).unwrap(),
-    );
-    new_request
-        .headers_mut()
-        .extend(std::mem::take(request.headers_mut()));
-    *new_request.body_mut() = Some(reqwest::Body::wrap_stream(
-        request.into_body().into_data_stream(),
-    ));
-
-    let mut resp = client
-        .execute(new_request)
-        .await
-        .map_err(|e| e.to_string())?;
-    let mut builder = axum::response::Response::builder().status(resp.status());
-
-    for (k, v) in std::mem::take(resp.headers_mut()) {
-        let Some(k) = k else {
-            continue;
-        };
-        builder = builder.header(k, v);
-    }
-
-    Ok(builder
-        .body(axum::body::Body::from(
-            resp.bytes().await.map_err(|e| e.to_string())?,
-        ))
-        .unwrap())
-}
 
 #[tokio::main]
 async fn main() {
@@ -64,8 +18,6 @@ async fn main() {
         .route("/ip", get(|ConnectInfo(addr): ConnectInfo<SocketAddr>| async move {
             format!("Your IP is {}", addr.ip())
         }))
-        .route("/lunabot", any(lunabot_redirect))
-        .with_state(Client::new())
         .route("/udp-ws", get(|ws: WebSocketUpgrade| async {
             ws.on_upgrade(|mut socket| async move {
                 let mut send_to = None;
@@ -128,6 +80,55 @@ async fn main() {
                                 continue;
                             };
                             send_to = Some(from);
+                            if let Err(e) = socket.send(Message::Binary(buf[..n].to_vec())).await {
+                                eprintln!("Failed to send UDP packet to client: {e}");
+                                break;
+                            }
+                        }
+                    }
+                }
+            })
+        }))
+        .route("/tcp-ws/:port", get(|ws: WebSocketUpgrade, Path((port,)): Path<(u16,)>| async move {
+            ws.on_upgrade(move |mut socket| async move {
+                let mut tcp = match TcpStream::connect(SocketAddrV4::new(Ipv4Addr::LOCALHOST, port)).await {
+                    Ok(x) => x,
+                    Err(e) => {
+                        eprintln!("Failed to connect to TCP server: {e}");
+                        return;
+                    },
+                };
+
+                let mut buf = [0u8; 2048];
+                loop {
+                    tokio::select! {
+                        option = socket.recv() => {
+                            let Some(result) = option else {
+                                break;
+                            };
+                            let msg = match result {
+                                Ok(msg) => msg,
+                                Err(e) => {
+                                    eprintln!("Received error from peer: {e}");
+                                    break;
+                                },
+                            };
+                            let Message::Binary(data) = msg else {
+                                continue;
+                            };
+                            if let Err(e) = tcp.write_all(&data).await {
+                                eprintln!("Failed to send TCP stream to client: {e}");
+                                break;
+                            }
+                        }
+                        result = tcp.read(&mut buf) => {
+                            let n = match result {
+                                Ok(n) => n,
+                                Err(e) => {
+                                    eprintln!("Failed to read TCP stream: {e}");
+                                    break;
+                                },
+                            };
                             if let Err(e) = socket.send(Message::Binary(buf[..n].to_vec())).await {
                                 eprintln!("Failed to send UDP packet to client: {e}");
                                 break;
