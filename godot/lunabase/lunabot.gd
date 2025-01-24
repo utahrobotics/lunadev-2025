99657ac2ff2bf8a41f5ca851437ce90e3fe853d6
manglemix <shabouza030@gmail.com>
2025-01-23 22:42:59 -0700

    websocket init draft

--- godot/lunabase/lunabot.gd
+++ godot/lunabase/lunabot.gd
@@ -1,5 +1,25 @@
 extends LunabotConn
 
+signal _disconnected
+
 enum State { AUTO, TELEOP, STOPPED }
 
 var current_state: State = State.STOPPED
+
+var init_ws: WebSocketPeer
+
+
+func _ready() -> void:
+	while true:
+		init_ws = WebSocketPeer.new()
+		if init_ws.connect_to_url("ws://192.168.0.100/init-lunabot") == OK:
+			await _disconnected
+		await get_tree().create_timer(5).timeout
+
+
+func _process(_delta: float) -> void:
+	if init_ws != null:
+		init_ws.poll()
+		if init_ws.get_ready_state() == WebSocketPeer.STATE_CLOSED:
+			init_ws = null
+			_disconnected.emit()
