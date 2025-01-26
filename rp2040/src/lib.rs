c44bf2a36dcd1d9ad5211f2954baf99d063da504
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-01-25 18:20:14 -0700

    changed boxed dyn Errors to std::io::Errors

--- rp2040/src/lib.rs
+++ rp2040/src/lib.rs
@@ -1,18 +1,20 @@
 use std::path::PathBuf;
 
 use embedded_common::*;
-use tokio::{self, fs, io::{AsyncReadExt, AsyncWriteExt}, process::Command};
+use tokio::{
+    self, fs,
+    io::{AsyncReadExt, AsyncWriteExt},
+    process::Command,
+};
 use tokio_serial::SerialStream;
 use tracing::{error, info};
 use udev::{Device, Entry, Enumerator, Udev};
 
 pub struct PicoController {
-    serial_port: SerialStream
+    serial_port: SerialStream,
 }
 
-
 impl PicoController {
-
     /// returns a sorted Vec of /dev/ttyACM* that have the serial number specified in embedded_common::ID_SERIAL
     pub fn enumerate_picos() -> Result<Vec<String>, std::io::Error> {
         let udev = Udev::new()?;
@@ -23,7 +25,9 @@ impl PicoController {
         for device in devices.collect::<Vec<Device>>() {
             if let Some(path) = device.devnode() {
                 device.properties().for_each(|property| {
-                    if property.value().to_string_lossy().to_string() == UDEVADM_ID && !path.starts_with("/dev/bus/") {
+                    if property.value().to_string_lossy().to_string() == UDEVADM_ID
+                        && !path.starts_with("/dev/bus/")
+                    {
                         let path = path.to_owned();
                         candidates.push(path.to_string_lossy().to_string());
                     }
@@ -34,13 +38,11 @@ impl PicoController {
         return Ok(candidates);
     }
 
-    pub async fn new(serial_port: &str) -> Result<Self, Box<dyn std::error::Error>> {
+    pub async fn new(serial_port: &str) -> Result<Self, std::io::Error> {
         let builder = tokio_serial::new(serial_port, 9600);
-        let file  = SerialStream::open(&builder)?;
+        let file = SerialStream::open(&builder)?;
         // let file = tokio::fs::OpenOptions::new().read(true).write(true).open(serial_port).await?;
-        Ok(PicoController{
-            serial_port: file
-        })
+        Ok(PicoController { serial_port: file })
     }
 
     // /// makes sure the serial number is Embassy_USB-serial_12345678
@@ -56,7 +58,7 @@ impl PicoController {
     //     }
     // }
 
-    async fn send_ack(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    async fn send_ack(&mut self) -> Result<(), std::io::Error> {
         let ack = [1];
         self.serial_port.write(&ack).await?;
         self.serial_port.flush().await?;
@@ -64,13 +66,16 @@ impl PicoController {
         Ok(())
     }
 
-    pub async fn get_message_from_pico(&mut self) -> Result<FromIMU, Box<dyn std::error::Error>> {
+    pub async fn get_message_from_pico(&mut self) -> Result<FromIMU, std::io::Error> {
         self.send_ack().await?;
         let mut data: [u8; 13] = [0; 13];
         let readcount = self.serial_port.read(&mut data).await?;
         return Ok(FromIMU::deserialize(data).map_err(|e| {
             error!("Error deserializing message FromIMU: {e}");
-            std::io::Error::new(std::io::ErrorKind::InvalidData, "failed to deserialize FromIMU from the serial port")
-        })?)
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "failed to deserialize FromIMU from the serial port",
+            )
+        })?);
     }
 }
