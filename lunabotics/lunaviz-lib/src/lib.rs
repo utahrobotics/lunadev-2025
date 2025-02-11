5af78ecb8ae6dae74e7f101fe4ad265e7dc75c75
Tyler Chin <tylerstyle05@gmail.com>
2025-02-10 22:28:00 -0700

    added images

--- lunabotics/lunaviz-lib/src/lib.rs
+++ lunabotics/lunaviz-lib/src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(let_chains)]
 
+use std::f128::consts::PI;
+
 use common::thalassic::{lunabase_task, ThalassicData};
 use godot::{classes::{Image, ImageTexture}, prelude::*};
 use parking_lot::Mutex;
@@ -37,18 +39,66 @@ impl INode for Lunaviz {
     }
 
     fn process(&mut self, _delta: f64) {
+
         if let Some(guard) = self.thalassic_data.try_lock()
             && guard.2
         {
             let thalassic_data = &guard.0;
             let point_cloud = guard.1.as_slice();
 
+            let mut y_count:i32 = 0;
+            let mut x_count = 0;
+            let mut obstacle_img = Image::new_gd();
+            let mut grad_img = Image::new_gd();
+            let mut height_img = Image::new_gd();
+            /*
+            loop {
+                loop{
+                    x_count-=1;
+                    if thalassic_data.expanded_obstacle_map[(y_count as usize*256)+x_count as usize].occupied(){
+                        obstacle_img.set_pixel(x_count, y_count, Color { r: (1f32), g: (1f32), b: (1f32), a: (1f32) });
+                    }
+                    if x_count>=128{
+                        break;
+                    }
+                }
+                y_count+=1;
+                if y_count>=128{
+                    break
+                }
+            }
+            */
+
+            let mut y_count:i32 = 0;
+            let mut x_count = 0;
+            for i in thalassic_data.heightmap{
+                let obstacle_val = thalassic_data.expanded_obstacle_map[(y_count as usize*256)+x_count as usize];
+                let grad_val:f32 = thalassic_data.gradmap[(y_count as usize*256)+x_count as usize];
+                let height_val:f32 = thalassic_data.heightmap[(y_count as usize*256)+x_count as usize];
+
+                if obstacle_val.occupied(){
+                    obstacle_img.set_pixel(x_count, y_count, Color { r: (1f32), g: (1f32), b: (1f32), a: (1f32) });
+                }else{
+                    obstacle_img.set_pixel(x_count, y_count, Color { r: (0f32), g: (0f32), b: (0f32), a: (0f32) });
+                }
+
+                let grad = (grad_val)/(3.14f32/2f32);
+                grad_img.set_pixel(x_count, y_count, Color { r: (grad), g: (grad), b: (grad), a: (grad) }); // 0 to pi/2
+                let height:f32 = (height_val+1f32)/2f32;
+                height_img.set_pixel(x_count, y_count, Color { r: (height), g: (height), b: (height), a: (height) }); // -1 to 1
+                x_count+=1;
+                if x_count>=256{
+                    x_count=0;
+                    y_count+=1;
+                }
+            }
+
             self.base_mut().emit_signal("send_map_data",
             &[Variant::from(Image::new_gd()),
             Variant::from(point_cloud),
             Variant::from(thalassic_data.heightmap),
-            Variant::from(thalassic_data.gradmap),
-            Variant::from(thalassic_data.expanded_obstacle_map)
+            Variant::from(grad_img),
+            Variant::from(obstacle_img)
             ]);
         }
     }
