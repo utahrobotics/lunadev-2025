83e39d6a7c6c34cc58b7adf421eb93bceb52244b
Najman Husaini <najmanhusaini20@gmail.com>
2025-02-19 15:33:29 -0700

    fixed teleop

--- lunabotics/lunabot-ai/src/teleop.rs
+++ lunabotics/lunabot-ai/src/teleop.rs
@@ -1,5 +1,7 @@
+use std::time::Duration;
+
 use ares_bt::{sequence::Sequence, Behavior, Status};
-use common::{FromLunabase, LunabotStage};
+use common::{FromLunabase, LunabotStage, Steering};
 use tracing::{error, warn};
 
 use crate::{
@@ -9,35 +11,48 @@ use crate::{
 };
 
 pub fn teleop() -> impl Behavior<LunabotBlackboard> {
+    let mut last_steering = Steering::default();
     Sequence::new((
         |blackboard: &mut LunabotBlackboard| {
             blackboard.enqueue_action(Action::SetStage(LunabotStage::TeleOp));
             Status::Success
         },
-        |blackboard: &mut LunabotBlackboard| {
+        move |blackboard: &mut LunabotBlackboard| {
             if *blackboard.lunabase_disconnected() {
                 error!("Lunabase disconnected");
                 return Status::Failure;
             }
-            while let Some(msg) = blackboard.pop_from_lunabase() {
-                match msg {
-                    FromLunabase::Steering(steering) => {
-                        blackboard.enqueue_action(Action::SetSteering(steering));
-                        return Status::Running;
+            macro_rules! handle {
+                ($msg: ident) => {
+                    match $msg {
+                        FromLunabase::Steering(steering) => {
+                            last_steering = steering;
+                        }
+                        FromLunabase::SoftStop => {
+                            warn!("Received SoftStop");
+                            return Status::Failure;
+                        }
+                        FromLunabase::TraverseObstacles => {
+                            *blackboard.get_autonomy() =
+                                Autonomy::PartialAutonomy(AutonomyStage::TraverseObstacles);
+                            return Status::Success;
+                        }
+                        _ => {}
                     }
-                    FromLunabase::SoftStop => {
-                        warn!("Received SoftStop");
-                        return Status::Failure;
-                    }
-                    FromLunabase::TraverseObstacles => {
-                        *blackboard.get_autonomy() =
-                            Autonomy::PartialAutonomy(AutonomyStage::TraverseObstacles);
-                        return Status::Success;
-                    }
-                    _ => {}
-                }
+                };
             }
-            *blackboard.get_poll_when() = PollWhen::ReceivedLunabase;
+            if let Some(msg) = blackboard.pop_from_lunabase() {
+                handle!(msg);
+            } else {
+                blackboard.enqueue_action(Action::SetSteering(last_steering));
+                *blackboard.get_poll_when() = PollWhen::Instant(blackboard.get_now() + Duration::from_millis(90));
+                return Status::Running;
+            }
+            while let Some(msg) = blackboard.pop_from_lunabase() {
+                handle!(msg);
+            }
+            blackboard.enqueue_action(Action::SetSteering(last_steering));
+            *blackboard.get_poll_when() = PollWhen::Instant(blackboard.get_now() + Duration::from_millis(90));
             Status::Running
         },
     ))
