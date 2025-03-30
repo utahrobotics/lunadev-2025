d13d7af1db62d6a7c24b33e307b5c2b6704ea2d3
manglemix <shabouza030@gmail.com>
2025-03-30 14:33:09 -0600

    temporary

--- lunabotics/lunabase-lib/src/audio.rs
+++ lunabotics/lunabase-lib/src/audio.rs
@@ -9,14 +9,13 @@ use godot::{
     global::godot_error,
     obj::{BaseMut, Gd, NewAlloc, NewGd},
 };
-use opus_static_sys::{opus_decode_float, OpusDecoder};
 
 use crate::LunabotConn;
 
 pub struct AudioStreaming {
     // playback: Gd<AudioStreamGeneratorPlayback>,
     udp: Option<UdpSocket>,
-    decoder: NonNull<OpusDecoder>,
+    decoder: NonNull<u32>,
     audio_buffer: [f32; AUDIO_FRAME_SIZE as usize],
     udp_buffer: [u8; 4096],
     player: Gd<AudioStreamPlayer>,
@@ -50,14 +49,15 @@ impl AudioStreaming {
         Self {
             udp,
             decoder: unsafe {
-                let mut err_code = 0;
-                let dec_ptr = opus_static_sys::opus_decoder_create(AUDIO_SAMPLE_RATE as i32, 1, &mut err_code);
+                // let mut err_code = 0;
+                // let dec_ptr = opus_static_sys::opus_decoder_create(AUDIO_SAMPLE_RATE as i32, 1, &mut err_code);
                 
-                let Some(dec) = NonNull::new(dec_ptr) else {
-                    panic!("Failed to create Opus decoder: {}", err_code);
-                };
+                // let Some(dec) = NonNull::new(dec_ptr) else {
+                //     panic!("Failed to create Opus decoder: {}", err_code);
+                // };
 
-                dec
+                // dec
+                todo!()
             },
             audio_buffer: [0.0; AUDIO_FRAME_SIZE as usize],
             udp_buffer: [0u8; 4096],
@@ -84,29 +84,30 @@ impl AudioStreaming {
                         let expected_i = self.expected_i.unwrap();
 
                         if i != expected_i {
-                            let result = unsafe {
-                                opus_decode_float(
-                                    self.decoder.as_ptr(),
-                                    std::ptr::null(),
-                                    0,
-                                    self.audio_buffer.as_mut_ptr(),
-                                    AUDIO_FRAME_SIZE as i32,
-                                    0
-                                )
-                            };
-                            godot_error!("Missed packet: {}", result);
+                            // let result = unsafe {
+                            //     opus_decode_float(
+                            //         self.decoder.as_ptr(),
+                            //         std::ptr::null(),
+                            //         0,
+                            //         self.audio_buffer.as_mut_ptr(),
+                            //         AUDIO_FRAME_SIZE as i32,
+                            //         0
+                            //     )
+                            // };
+                            // godot_error!("Missed packet: {}", result);
                         }
 
                         self.expected_i = Some(i + 1);
                         let result = unsafe {
-                            opus_decode_float(
-                                self.decoder.as_ptr(),
-                                self.udp_buffer.as_ptr().add(4).cast(),
-                                n as i32 - 4,
-                                self.audio_buffer.as_mut_ptr(),
-                                AUDIO_FRAME_SIZE as i32,
-                                0
-                            )
+                            // opus_decode_float(
+                            //     self.decoder.as_ptr(),
+                            //     self.udp_buffer.as_ptr().add(4).cast(),
+                            //     n as i32 - 4,
+                            //     self.audio_buffer.as_mut_ptr(),
+                            //     AUDIO_FRAME_SIZE as i32,
+                            //     0
+                            // )
+                            1
                         };
                         if result < 0 {
                             godot_error!("Failed to decode audio: {}", -result);
@@ -134,14 +135,6 @@ impl AudioStreaming {
                     }
                 }
             }
-        }
-    }
-}
-
-impl Drop for AudioStreaming {
-    fn drop(&mut self) {
-        unsafe {
-            opus_static_sys::opus_decoder_destroy(self.decoder.as_ptr());
         }
     }
 }
