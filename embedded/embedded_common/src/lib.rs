3d3de631bf0831509aaed93ce62d0e7f0fdaf260
Matthew Ashton Knochel <matthewashtonknochel@gmail.com>
2025-04-06 21:52:24 -0600

    updated common lib

--- embedded/embedded_common/src/lib.rs
+++ embedded/embedded_common/src/lib.rs
@@ -1,5 +1,22 @@
 #![no_std]
 
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[repr(u8)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub enum Direction {
+    Forward = 0,
+    Backward = 1,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+#[repr(u8)]
+pub enum ActuatorCommand {
+    SetSpeed(u16) = 0,
+    SetDirection(Direction) = 1,
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub enum FromIMU {
@@ -96,6 +113,47 @@ impl FromIMU {
             2 => Ok(FromIMU::NoDataReady),
             3 => Ok(FromIMU::Error),
             _ => Err("Invalid variant tag")
+        }
+    }
+}
+impl ActuatorCommand {
+    pub fn deserialize(bytes: [u8; 3]) -> Result<Self, &'static str> {
+        match bytes[0] {
+            tag if tag == ActuatorCommand::SetSpeed as u8 => {
+                let speed = u16::from_le_bytes(
+                    bytes[1..]
+                        .try_into()
+                        .map_err(|_| "Wrong number of bytes in actuator command")?,
+                );
+                Ok(ActuatorCommand::SetSpeed(speed))
+            }
+            tag if tag == ActuatorCommand::SetDirection as u8 => {
+                let dir = match bytes[1] {
+                    0 => Direction::Forward,
+                    1 => Direction::Backward,
+                    _ => return Err("Invalid direction value"),
+                };
+                Ok(ActuatorCommand::SetDirection(dir))
+            }
+            _ => Err("Invalid variant tag"),
+        }
+    }
+
+    pub fn serialize(&self) -> [u8; 3] {
+        match self {
+            ActuatorCommand::SetSpeed(speed) => {
+                let mut bytes = [0u8; 3];
+                bytes[0] = ActuatorCommand::SetSpeed as u8;
+                bytes[1..].copy_from_slice(&speed.to_le_bytes());
+                bytes
+            }
+            ActuatorCommand::SetDirection(dir) => {
+                let mut bytes = [0u8; 3];
+                bytes[0] = ActuatorCommand::SetDirection as u8;
+                bytes[1] = *dir as u8;
+                bytes[2] = 0;
+                bytes
+            }
         }
     }
 }
